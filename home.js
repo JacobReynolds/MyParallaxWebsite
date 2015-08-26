ce05ebe1de0e1a84b4cfa4cae9a41e8458724f74
Jake Reynolds <reyno511@umn.edu>
2015-08-25 20:26:42 -0500

    ls -l added

--- home.js
+++ home.js
@@ -307,7 +307,7 @@ $(document).ready(function () {
             $("#terminalOutput").append('Invalid command \"' + command + '"<br>type "help" for more options<br>');
             addInput();
         }
-        if (input === 'ls -la' || input === 'ls -a' || input === 'ls -all') {
+        if (input === 'ls -la' || input === 'ls -a' || input === 'ls -all' || input === 'ls -l') {
             printAllFiles();
             return;
         }
